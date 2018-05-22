97ce65e5db8fc8396fa619c46f4d27b2aba79f5a
Chris Azzara <cazzara91@gmail.com>
2018-05-22 11:57:35 -0400

    Added hex output to ipask.py

--- day2/ipask.py
+++ day2/ipask.py
@@ -9,3 +9,6 @@ mac = input("Enter your MAC Address: ")
 
 # Display Info
 print("Your IP Address is {} and your MAC Address is {}".format(ip, mac))
+octets = ip.split(".")
+octets = map(int, octets)
+print("Your IP Address in Hex: {:02X}{:02X}{:02X}{:02X}".format(*octets))
