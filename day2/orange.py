8a2d494c7e6e25becff0cf17b909db4425885b3a
Chris Azzara <cazzara91@gmail.com>
2018-05-22 11:14:40 -0400

    Practice manipulating the print function

---
+++ day2/orange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Chris Azzara
+# Purpose: Practice manipulating the print function parameters
+
+print("O", sep="", end="") 
+print("R", sep="", end="")
+print("A", sep="", end="")
+print("N", sep="", end="")
+print("G", sep="", end="")
+print("E", sep="")
