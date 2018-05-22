77a42fd73024c84597601fbd64e3e353fa1ea1e3
Chris Azzara <cazzara91@gmail.com>
2018-05-22 11:38:35 -0400

    Added ipask.py

---
+++ day2/ipask.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Chris Azzara
+# Purpose: Ask user for IP and MAC address and display it back to them
+
+# Collect User Input
+ip = input("Enter your IP Address: ")
+mac = input("Enter your MAC Address: ")
+
+# Display Info
+print("Your IP Address is {} and your MAC Address is {}".format(ip, mac))
--- day2/orange.py
+++ day2/orange.py
@@ -8,3 +8,10 @@ print("A", sep="", end="")
 print("N", sep="", end="")
 print("G", sep="", end="")
 print("E", sep="")
+
+fruit = "ORANGE"
+
+# Formatting and Aligning Text
+print("{:-<30}".format(fruit)) # Left Aligned
+print("{:->30}".format(fruit)) # Right Aligned
+print("{:-^30}".format(fruit)) # Centered
